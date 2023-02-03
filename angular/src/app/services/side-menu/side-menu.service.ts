0a27a1e54d9a84fce94bf245a066f1584f6a443d
Mateusz <mtpala@interia.pl>
2023-02-03 22:07:04 +0100

    Przeniesienie zakladki powiadomien uzytkownikow do administracji

--- angular/src/app/services/side-menu/side-menu.service.ts
+++ angular/src/app/services/side-menu/side-menu.service.ts
@@ -11,18 +11,54 @@ export class SideMenuService {
   public getSideMenuTabs(): SideMenuModel[] {
     return [
       {
-        label: 'RCP', expanded: false, routing: null, roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'],
+        label: 'RCP',
+        expanded: false,
+        routing: null,
+        roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'],
         childs: [
-          {label: 'Zgłoszenie obecności w pracy', expanded: false, routing: 'attendance-work-report', roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'], childs: null},
-          {label: 'Wnioski urlopowe', expanded: false, routing: 'holiday-request', roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'], childs: null},
-          {label: 'Akceptacja wniosków urlopowych', expanded: false, routing: 'holiday-request-confirmation', roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR'], childs: null},
-          {label: 'Powiadomienie użytkowników', expanded: false, routing: 'new-annotation', roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR'], childs: null}
+          {
+            label: 'Zgłoszenie obecności w pracy',
+            expanded: false,
+            routing: 'attendance-work-report',
+            roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'],
+            childs: null
+          },
+          {
+            label: 'Wnioski urlopowe',
+            expanded: false,
+            routing: 'holiday-request',
+            roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR', 'ROLE_WORKER'],
+            childs: null
+          },
+          {
+            label: 'Akceptacja wniosków urlopowych',
+            expanded: false,
+            routing: 'holiday-request-confirmation',
+            roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR'],
+            childs: null
+          }
         ]
       },
       {
-        label: 'Administracja', expanded: false, routing: null, roles: ['ROLE_ADMIN'],
+        label: 'Administracja',
+        expanded: false,
+        routing: null,
+        roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR'],
         childs: [
-          {label: 'Użytkownicy', expanded: false, routing: 'users', roles: ['ROLE_ADMIN'], childs: null}
+          {
+            label: 'Użytkownicy',
+            expanded: false,
+            routing: 'users',
+            roles: ['ROLE_ADMIN'],
+            childs: null
+          },
+          {
+            label: 'Powiadomienie użytkowników',
+            expanded: false,
+            routing: 'new-annotation',
+            roles: ['ROLE_ADMIN', 'ROLE_EMPLOYER', 'ROLE_EMPLOYER_SUPERVISOR'],
+            childs: null
+          }
         ]
       }
     ]

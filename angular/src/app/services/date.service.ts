dceda96610a4860133b853d846aec79ab87b9889
Mateusz <mtpala@interia.pl>
2023-02-04 22:50:31 +0100

    Dodano nową metode w dateService + rename klasy serwisowej

--- angular/src/app/services/date.service.ts
+++ angular/src/app/services/date.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {DatePipe} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DateFormatterService {
+export class DateService {
 
   constructor(private datePipe: DatePipe) {
   }
 
   public convertDateToJavaLocalDate(date: Date) {
     return this.datePipe.transform(date, 'yyyy-MM-dd');
+  }
+
+  public calculateDiffrenceBetweenDatesInDays(dateFrom: Date, dateTo: Date) {
+    this.zeroDate(dateFrom);
+    this.zeroDate(dateTo);
+    const dateToPlusOne = new Date(dateTo.setDate(dateTo.getDate() + 1));
+    const diffrenceInTime = dateToPlusOne.getTime() - dateFrom.getTime();
+    return Math.floor(diffrenceInTime / (1000 * 3600 * 24));
+  }
+
+  private zeroDate(date: Date) {
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setMilliseconds(0);
   }
 
 }

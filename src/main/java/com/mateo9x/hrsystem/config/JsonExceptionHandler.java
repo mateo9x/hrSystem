3a76a7e3fddef896a96977372dd62f73e2a6e715
Mateusz <mtpala@interia.pl>
2023-02-10 09:49:00 +0100

    Włączono logowanie wyjątków

--- src/main/java/com/mateo9x/hrsystem/config/JsonExceptionHandler.java
+++ src/main/java/com/mateo9x/hrsystem/config/JsonExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.mateo9x.hrsystem.config;
 
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class JsonExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<Object> handleAllOtherErrors(Exception exception) {
+        log.error(exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorResponse(exception.getMessage()));
